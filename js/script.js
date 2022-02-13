3b14c7ad809cd67f503c80b04cb290b6ef06be8d
Sheshkon <sheskoalexei@gmail.com>
2022-02-13 12:54:05 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const URL_PARAMS = new URLSearchParams(window.location.search);
 const ROOM = URL_PARAMS.get('room');
-console.log(ROOM);
 if(ROOM)
     window.location.replace('tetris://${ROOM}');
+    console.log('tetris://${ROOM}');

ea58652341a5e077d053484be453ae2b38d405c8
Alexei <sheskoalexei@gmail.com>
2022-03-06 17:30:25 +0300

    added table next and prev pages

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -2,11 +2,18 @@ const socket = io('https://salty-fjord-01783.herokuapp.com', {
     rejectUnauthorized: false,
 });
 const place_color = ["#FFD700", "#C0C0C0", "#CD7F32"];
-
+const TABLE_PAGE_HEIGHT = 5;
 var users = [];
+var pos = 0;
+var lim_pos = 1;
 var table = document.getElementById('leaders_table');
 var leaderboard = document.getElementById('leaderboard');
+var table_body = document.getElementById("table_body");
+var left_arrow = document.getElementById("show_prev");
+var right_arrow = document.getElementById("show_next");
 leaderboard.hidden = true;
+left_arrow.style.opacity = 0;
+right_arrow.style.display = 0;
 
 
 socket.on('connect', function () {
@@ -15,7 +22,7 @@ socket.on('connect', function () {
     socket.on("leaderboard", players => {
         console.log('get leaderboard');
         users = players;
-        fill_table();
+        show_table();
     });
 });
     
@@ -24,15 +31,27 @@ socket.on("connect_error", (err) => {
 });
 
 
-function fill_table() {
-    var table_body = document.getElementById("table_body");
+function show_table() {
+    lim_pos = Math.floor(users.length / TABLE_PAGE_HEIGHT);
+    var end_pos = (users.length <= TABLE_PAGE_HEIGHT) ? users.length: TABLE_PAGE_HEIGHT;
+    if (users.length > TABLE_PAGE_HEIGHT){
+        
+    }
+
+    fill_table(0, end_pos)
+    leaderboard.hidden = false;
+}
+
+function fill_table(start_pos, end_pos){
     table_body.innerHTML = '';
-    for (var i = 0; i < users.length; i++) {
+    
+    for (var i = start_pos; i < end_pos; i++) {
         var obj = users[i];
         font_color = "white";
-        if (i < 3){
+
+        if (i < 3)
             font_color = place_color[i];
-        }
+
         var row = `<tr><th scope="row"><font color=${font_color}>` + (i + 1) + '</font></th>';
         
         for (var key in obj) {
@@ -40,8 +59,43 @@ function fill_table() {
             row += (`<td><font color=${font_color}>` + val + '</font></td>');
         }
         row += ('</tr>');
-        $(row).appendTo(table_body)
-
+        $(row).appendTo(table_body);
     }
-    leaderboard.hidden = false;
+    left_arrow.style.opacity = 0;
+    right_arrow.style.opacity = 0;
+
+    if (pos > 0)
+        left_arrow.style.opacity = 1;
+
+    if (pos >=0 && end_pos != users.length)
+        right_arrow.style.opacity = 1;
+
+    
+    
+}
+
+
+function show_next(start_pos, end_pos){
+    if (pos + 1 <= lim_pos)
+        pos += 1;
+    var start_pos = pos*TABLE_PAGE_HEIGHT;
+    var end_pos = (pos == lim_pos) ? users.length: start_pos + TABLE_PAGE_HEIGHT;
+    console.log(start_pos, end_pos);
+    console.log(lim_pos);
+    if (start_pos != end_pos)
+        fill_table(start_pos, end_pos);
+
+
+    
+}
+
+function show_prev(){
+    if (pos - 1 >= 0)
+        pos-=1;
+
+    var start_pos = pos*TABLE_PAGE_HEIGHT;
+    var end_pos = start_pos + TABLE_PAGE_HEIGHT;
+    fill_table(start_pos, end_pos)
+
+
 }
